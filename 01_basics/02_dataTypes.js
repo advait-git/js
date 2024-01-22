d13da20cec835b15f82589a2d82c91fd8c7a0c9f
advait tiwari <109410550+advait-git@users.noreply.github.com>
2024-01-22 13:58:13 +0000

    variables and data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+let name ="advait" //string
+let age=22 //int
+let isLogged=false //boolean
+//null => khali
+let state; //undefined
+
+//type of null is object or some say it is an error
+//type of undefined is undefined
