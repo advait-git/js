3477e9e00292e979de012ed4ace72936d43a8663
advait tiwari <109410550+advait-git@users.noreply.github.com>
2024-01-22 14:15:05 +0000

    conversion od data types

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,12 @@
+let score="33"
+console.log(typeof score);
+let valueinNo = Number (score) //changed it to no. from string
+console.log(typeof valueinNo);
+//1-> true 0-> false ""->false "abc..."->true in boolean
+
+let some = 18
+console.log(some)
+let s=String(some)
+console.log(some)
+console.log(typeof some)
+//similarly we can change it to boolean value also or vise versa
