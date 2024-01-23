4cdd1ce410bcc03cf1e554f79625a0340a2d1a98
advait tiwari <109410550+advait-git@users.noreply.github.com>
2024-01-23 16:30:00 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,8 @@
+//primitive : call by value
+//there r 7 :String , boolean , number , null, undefined, symbol ,bigint
+
+//data type we use typeof and null is object type and undefined is undefined
+
+//non primitive : call by reference
+//Array , Objects , Functions
+//datatypes of non primitive is functions only but the function : will have function object as datatype
