fc46eabb33d51b85686a0f6abe1f043e279d1b70
advait tiwari <109410550+advait-git@users.noreply.github.com>
2024-01-27 14:11:17 +0000

    array1

---
+++ 02_basics/arrays.js
@@ -0,0 +1,20 @@
+const arr=[0,1,2,3,4];
+const name=["adv","ak","rohan"]
+const my=new Array(1,2,3,4)
+console.log(arr[1])
+//array methodes
+
+arr.push(7)
+arr.pop()
+
+//.unshift(no.) will push that no. at first
+//.shift() will remove the first no.
+const newarr=arr.join(); //convert arr into string type
+
+console.log(arr);
+console.log(newarr);
+
+//slice help us to take out the portion which we want to take from the main array
+//splice changes the main array it takes out the portion we want from the main orignal array 
+
+
